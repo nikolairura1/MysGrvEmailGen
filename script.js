658a245190b4fd156d4b3462c48ba8a58fcacbc1
nikolairura1 <nikola.e.rura@gmail.com>
2025-02-11 04:01:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ let infowindow;
 let userCity = "your city"; // Default placeholder
 
 const bookDescriptions = {
-    "Storm of Steel": "Storm of Steel is a memoir of World War I, written by Ernst Jünger, detailing his experiences as a German officer on the Western Front",
-    "Always with Honor": "Always with Honor is a memoir by Pyotr Wrangel, recounting his time as a White Army general during the Russian Civil War",
-    "Peter Kemp Trilogy": "The Peter Kemp Trilogy covers the author's experiences fighting in the Spanish Civil War and later serving in British special forces during WWII",
-    "Conrad Masterworks": "A collection of masterful writings by Joseph Conrad, exploring themes of colonialism, human nature, and adventure on the high seas",
-    "Oblensky Memoir": "A memoir of Prince Oblensky, detailing his life as an aristocrat, soldier, and exile after the Russian Revolution"
+    "Storm of Steel": "the experiences of Ernest Junger, a German Officer, on the Western Front in World War One. The 1929 translation was out of print for a while but reputedly is the best at bringing across the vivid depictions of war that Junger is famous for. This was the version I was interested in (https://www.amazon.com/Storm-Steel-Original-1929-Translation/dp/1696237726)",
+    "Always with Honor": "the life by Pyotr Wrangel, a White Army general during the Russian Civil War, and offers a really unique perspective on one of the most important but least studied conflicts of the 20th Century. There's been something of a revival in RCW interest recently and I think this book has been a big part of it",
+    "Peter Kemp Trilogy": "the author's experiences fighting in the Spanish Civil War and later serving in British special forces during WWII, and its got three volumes, each for a different war",
+    "Conrad Masterworks": "exploring themes of colonialism, human nature, and adventure on the high seas, and Conrad is a classic author. I like this collection (https://www.amazon.com/Joseph-Conrad-Masterworks-Vol-I/dp/B0BMJQQWZL/ref=pd_bxgy_thbs_d_sccl_2/134-1489599-8882142?pd_rd_w=4adub&content-id=amzn1.sym.52395280-70da-442b-acda-fca52ac79a0b&pf_rd_p=52395280-70da-442b-acda-fca52ac79a0b&pf_rd_r=YPQH8WD19072W0GK8AQY&pd_rd_wg=3LOCB&pd_rd_r=57ec0e9b-2cdd-4927-ae5f-eb320fba2066&pd_rd_i=B0BMJQQWZL&psc=1) particularly because it includes some of his lesser known short stories",
+    "Oblensky Memoir": "the life of Prince Oblensky, detailing his life as an aristocrat, soldier, and exile after the Russian Revolution. There has been a revival in interest in the Russian Civil War recently, and I think this book would be interesting to everyone who wants to learn about the Whites in that war"
 };
 fetch('/.netlify/functions/getapikey')
   .then(response => response.json())
@@ -103,7 +103,7 @@ function generateEmail(libraryName, libraryContact) {
 
 I am a resident of ${userCity} and an occasional patron of ${libraryName}, and I'm writing to find out if you would be willing to stock "${selectedBook}". I checked your catalog and you don't seem to have it in stock already. I (read it recently OR am interested in reading it) and I really think it would be of interest to history lovers in our community. 
 
-The book is about ${bookDescription}. 
+It's about ${bookDescription}. 
 
 Please let me know if I just missed it in the catalog, or if there is a different process to request books to be stocked. 
 
